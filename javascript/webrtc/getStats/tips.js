18e7c57fbe2726a6535b66a15bd2480ceb2e32af
yuyake0084 <yuyake0084@gmail.com>
2019-11-15 09:59:09 +0900

    Create tips.js

---
+++ javascript/webrtc/getStats/tips.js
@@ -0,0 +1,11 @@
+/**
+ * statsの整形
+ * この時点で取得したstatsはデータ構造が複雑なので、操作しやすくする為に配列に変換。
+ */
+async function getStats() {
+  const stats = _pc.getStats()
+  const fixStats = Array.from(stats).map(stat => stat[1])
+  
+  return stats
+}
+
