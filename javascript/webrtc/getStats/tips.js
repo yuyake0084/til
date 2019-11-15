9bca27f6bbe6049fa8a03094e93f2a527f114c61
yuyake0084 <yuyake0084@gmail.com>
2019-11-15 10:00:06 +0900

    Update tips.js

--- javascript/webrtc/getStats/tips.js
+++ javascript/webrtc/getStats/tips.js
@@ -2,7 +2,7 @@
  * statsの整形
  * この時点で取得したstatsはデータ構造が複雑なので、操作しやすくする為に配列に変換。
  */
-async function getStats() {
+async function getFixStats(_pc) {
   const stats = _pc.getStats()
   const fixStats = Array.from(stats).map(stat => stat[1])
   
